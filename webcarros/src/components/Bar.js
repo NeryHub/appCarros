0f4f4149bf849d7033fca7b49e8dddd31aa789ae
NeryHub <125278013+NeryHub@users.noreply.github.com>
2024-08-21 08:51:50 +0200

    Atualizar o Bar.js

--- webcarros/src/components/Bar.js
+++ webcarros/src/components/Bar.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import 
 
 export default function Bar() {
   return (
-    <div>Bar</div>
+    <div>
+<h1> Find Your Car </h1>
+<input type="text" id="searchBar" placeholder="Pesquisar...">
+
+    <ul id="results"></ul>
+
+</div>
   )
 }
